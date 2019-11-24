6ab00bbbd7b02ae53f6d7bb008b602c18312c442
Arthur Makarenko <makarthur914@yahoo.com>
2019-11-24 09:53:21 +0000

    Automatically backed up by Learn

---
+++ sample_codes.rb
@@ -0,0 +1,32 @@
+# Time.now.year
+
+# puts "hey you know what year it is?"
+# this_year = Time.now.year - 10
+# puts "Hey, its #{this_year}"
+# if 2019
+#     puts "can't wait for 2020"
+#   elsif (this_year > 2019)
+#     puts "how was/is 2020"
+#   else (this_year < 2019)
+#     puts "almost there"
+# end
+
+
+
+# hello = true
+
+# case hello
+#   when hello
+#     puts "yello"
+#   when goodbye
+#     puts "goodbye"
+# end
+
+
+# coat = "black"
+
+
+# while "coat = black" 3. times do
+# puts "thats my coat"
+# end
+# 
---
+++ test.rb
@@ -0,0 +1,8 @@
+
+name = "babechick"
+
+def love_letter 
+  puts "hello my love. i swear i remember your name is #{name}"
+end
+
+love_letter
